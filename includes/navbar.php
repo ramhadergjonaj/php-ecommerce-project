6c386a416440d7b87c096d4cddebf29ab2ada760
Ramë Hadergjonaj <rame.hadergjonaj@student.uni-pr.edu>
2026-04-25 01:40:23 +0200

    feat: add navigation bar

---
+++ includes/navbar.php
@@ -0,0 +1,10 @@
+<nav class="navbar">
+    <div class="nav-container">
+        <a href="/" class="logo">PHP Shop</a>
+        <ul class="nav-links">
+            <li><a href="/">Home</a></li>
+            <li><a href="/products.php">Products</a></li>
+            <li><a href="/cart.php">Cart</a></li>
+        </ul>
+    </div>
+</nav>
