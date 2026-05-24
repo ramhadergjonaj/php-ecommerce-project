50fa6f22d5c702ea4cb3ecfbc631fb3917b5ad17
shuajb <shuajb.bytyqi@gmail.com>
2026-05-24 11:56:28 +0200

    shtimi i categories tek admin

--- pages/admin/categories.php
+++ pages/admin/categories.php
@@ -128,4 +128,7 @@ require_once __DIR__ . '/../../includes/header.php';
     </div>
 </div>
 
-<?php require_once __DIR__ . '/../../includes/footer.php'; ?>
+<?php require_once __DIR__ . '/../../includes/footer.php'; ?> 
+
+
+//categories
